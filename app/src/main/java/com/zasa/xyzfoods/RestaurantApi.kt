df2bacd34241015fcd7a21ee72c53c47ce02794e
zansangeeth <zansangeeth17@gmail.com>
2022-09-09 10:41:04 +0530

    created data class and responce and api call

---
+++ app/src/main/java/com/zasa/xyzfoods/RestaurantApi.kt
@@ -0,0 +1,14 @@
+package com.zasa.xyzfoods
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+/**
+ **@Project -> XYZ Foods
+ **@Author -> Sangeeth on 9/9/2022
+ */
+
+interface RestaurantApi {
+    @GET("branches.json")
+    fun getBranches() : Call<BranchResult>
+}
---
+++ app/src/main/java/com/zasa/xyzfoods/Utils.kt
@@ -0,0 +1,9 @@
+package com.zasa.xyzfoods
+
+/**
+ **@Project -> XYZ Foods
+ **@Author -> Sangeeth on 9/9/2022
+ */
+object Utils {
+    const val BASE_URL = "https://payable-training-evaluation.firebaseio.com/"
+}

0848a327884419e5613787de72060fc1cc0dc40d
zansangeeth <zansangeeth17@gmail.com>
2022-09-09 10:34:38 +0530

    Initial commit

---
+++ app/src/main/java/com/zasa/xyzfoods/BranchResult.kt
@@ -0,0 +1,10 @@
+package com.zasa.xyzfoods
+
+data class BranchResult(
+    val description: String,
+    val id: String,
+    val image_url: String,
+    val latitude: Double,
+    val longitude: Double,
+    val name: String
+)
